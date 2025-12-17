54b66eda54972291f66e86c2352ea7619db684fd
vansh-09 <vanshkjain09@gmail.com>
2025-12-17 23:35:19 +0530

    proxy code incomplete

---
+++ src/proxy.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import { redis } from "@/lib/redis";
+
+export const proxy = async <{connected; String[]; createdAt: number}>(`meta:${roomId}`);
+
+  //if user allowed in the room, let them pass, if not, send to lobby
+};
+
+export const config = {
+  macthcer: "/room/path*",
+};
